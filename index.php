f344b753483a48c7e65d48c92323cfa6b29297e1
Cholwe Muleya <cholwemuleya1@gmail.com>
2024-10-14 22:12:08 +0100

    Services section complete

--- index.php
+++ index.php
@@ -94,7 +94,7 @@
                     <a href="https://www.linkedin.com/company/biyashara-innovation-limited/posts/?feedView=all" target="_blank">
                         <h2><small>(02/2024 - Present)</small> Biyashara Innovations Limited<i class='bx bx-link-external'></i></h2>
                     </a>
-                    <p>A company specialised in in crafting tailored solutions that precisely align with the client's unique needs and 
+                    <p>A company specialised in in crafting tailored solutions that precisely align with the client's unique needs and
                         objectives. I was able to put the skills I have learned in real life scenarios and projects, further sharpening
                         my problem-solving and personal software development skills.</p>
                 </div>
@@ -108,7 +108,7 @@
                     <a href="https://nuyew.academy" target="_blank">
                         <h2><small>(07/2024 - Present)</small> NuYew Tech Academy <i class='bx bx-link-external'></i></h2>
                     </a>
-                    <p>A company that supports you to achieve your dream career in tech. Through modules, labs, exams and 
+                    <p>A company that supports you to achieve your dream career in tech. Through modules, labs, exams and
                         self-paced learning, I can work towards my childhood passion in pursuing a career in Cybersecurity, pushing
                         myself to unlock my full potenital.
                     </p>
@@ -118,6 +118,100 @@
     </section>
 
     <!-- education section ends -->
+
+    <!-- services section starts -->
+
+    <section class="services" id="services">
+        <h2 class="heading">Services</h2>
+
+        <div class="services-container">
+            <div class="service-box">
+                <i class="bx bx-code-alt"></i>
+                <div class="service-info">
+                    <h4>Web Development</h4>
+                    <p>Bringing a blend of technical expertise and creative
+                        problem-solving to the table. Proficient in a diverse range
+                        of programming languages and frameworks, I specialize
+                        in crafting robust and scalable web solutions.
+                        With a keen eye for detail and a passion for user-centric design,
+                        I strive to deliver not just functional websites, but exceptional
+                        digital experiences.</p>
+                </div>
+            </div>
+
+            <div class="service-box">
+                <i class="bx bx-data"></i>
+                <div class="service-info">
+                    <h4>Database Administration</h4>
+                    <p> Bringing a wealth of expertise and dedication to ensuring the seamless
+                        management and optimization of your data infrastructure. Proficient in
+                        designing, implementing, and maintaining robust database systems,
+                        I specialize in enhancing performance, scalability, and security.
+                        Partner with me to unlock the full potential of your data assets, with a
+                        focus on precision, security, and performance in every aspect of database
+                        administration.</p>
+                </div>
+            </div>
+
+            <div class="service-box">
+                <i class="bx bx-terminal"></i>
+                <div class="service-info">
+                    <h4>Penetration Testing</h4>
+                    <small>(Coming Soon!)</small>
+                    <p>Bringing a comprehensive approach to fortifying your digital defenses. My expertise
+                        lies in proactively identifying and addressing vulnerabilities in your
+                        systems, applications, and networks, ensuring they withstand the
+                        ever-evolving threat landscape. With a methodical and ethical hacking
+                        mindset, I conduct thorough assessments to expose potential weaknesses
+                        and provide actionable insights for remediation.</p>
+                </div>
+            </div>
+
+            <div class="service-box">
+                <i class='bx bx-laptop'></i>
+                <div class="service-info">
+                    <h4>Software Solutions</h4>
+                    <p>Bringing a blend of technical expertise and creative
+                        problem-solving to the table. Proficient in a diverse range
+                        of programming languages and frameworks, I specialize
+                        in crafting robust and scalable web solutions.
+                        With a keen eye for detail and a passion for user-centric design,
+                        I strive to deliver not just functional websites, but exceptional
+                        digital experiences.</p>
+                </div>
+            </div>
+
+            <div class="service-box">
+                <i class='bx bx-desktop'></i>
+                <div class="service-info">
+                    <h4>Hardware Solutions</h4>
+                    <p>Bringing a blend of technical expertise and creative
+                        problem-solving to the table. Proficient in a diverse range
+                        of programming languages and frameworks, I specialize
+                        in crafting robust and scalable web solutions.
+                        With a keen eye for detail and a passion for user-centric design,
+                        I strive to deliver not just functional websites, but exceptional
+                        digital experiences.</p>
+                </div>
+            </div>
+
+            <div class="service-box">
+                <i class='bx bxl-windows'></i>
+                <div class="service-info">
+                    <h4>OS Solutions</h4>
+                    <p>Bringing a blend of technical expertise and creative
+                        problem-solving to the table. Proficient in a diverse range
+                        of programming languages and frameworks, I specialize
+                        in crafting robust and scalable web solutions.
+                        With a keen eye for detail and a passion for user-centric design,
+                        I strive to deliver not just functional websites, but exceptional
+                        digital experiences.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- services section ends -->
 
 
     <!-- js file link -->

7615f770feda3db674bd2a0b43c54e34a73599a0
Cholwe Muleya <cholwemuleya1@gmail.com>
2024-10-14 11:17:43 +0100

    Header section complete

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cholwe Muleya</title>
+
+    <!-- box icons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- css file link -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/x-icon" href="images/logo1.jpg">
+</head>
+
+<body>
+
+    <!-- header section starts -->
+    <header class="header">
+        <a href="#" class="logo">Cholwe
+            <span>Muleya</span>
+        </a>
+
+        <!-- menu icon -->
+        <div class="bx bx-menu" id="menu-icon"></div>
+
+        <!-- navigation bar -->
+        <nav class="navbar">
+            <a href="#">Home</a>
+            <a href="#">Education</a>
+            <a href="#">Services</a>
+            <a href="#">Certifications</a>
+            <a href="#">Contact</a>
+        </nav>
+    </header>
+    <!-- header section ends -->
+
+
+
+
+
+    <!-- js file link -->
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>

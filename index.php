96149e425b1ede57988a7c2e2a609ed8f5db8c8a
Cholwe Muleya <cholwemuleya1@gmail.com>
2024-10-14 14:09:36 +0100

    Home section complete

--- index.php
+++ index.php
@@ -37,6 +37,34 @@
     <!-- header section ends -->
 
 
+    <!-- home section begins -->
+
+    <section class="home" id="home">
+        <div class="home-content">
+            <h1>Hi 👋🏾, I'm <span>Cholwe</span></h1>
+            <h3 class="text-animation">I'm <span></span></h3>
+            <p>Empowering the future with keystrokes, shaping tomorrow through web development and ethical hacking –
+                where innovation meets integrity in the digital realm.</p>
+
+            <div class="social-icons">
+                <a href="https://twitter.com/cholwe_muleya" target="_blank"><i class='bx bxl-twitter'></i></a>
+                <a href="https://www.linkedin.com/in/cholwe-muleya-198a861b2/" target="_blank"><i class='bx bxl-linkedin-square'></i></a>
+                <a href="https://www.instagram.com/charlie_m_kirk/" target="_blank"><i class='bx bxl-instagram'></i></a>
+                <a href="https://github.com/Shadw-007" target="_blank"><i class='bx bxl-github'></i></a>
+            </div>
+
+            <div class="btn-group">
+                <a href="#contact" class="btn">Hire</a>
+                <a href="cv/Cholwe_Muleya_CV.pdf" class="btn" download>Download CV</a>
+            </div>
+        </div>
+            <div class="home-img">
+                <img src="images/cholwe1.png" alt="">
+            </div>
+    </section>
+
+    <!-- home section ends -->
+
 
 
 
